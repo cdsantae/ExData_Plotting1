e54c6ed1514f9b8d3012f00e884202f8659daac5
Cristian Santa <cdsantae@users.noreply.github.com>
2015-05-10 12:52:10 -0500

    Create Plot2.R

---
+++ Plot2.R
@@ -0,0 +1,28 @@
+#Preparing dataset for plots
+setwd(".../Exploratory Data Analysis/Course Project 1")
+
+#Read original Data
+data<-read.table("household_power_consumption.txt",sep=";",header=T,stringsAsFactors=F)
+
+#Change data class of the variables
+data$Date<-strptime(paste(data$Date,data$Time,sep=" "),"%d/%m/%Y %H:%M:%S")
+data$Global_active_power<-as.numeric(data$Global_active_power)
+data$Global_reactive_power<-as.numeric(data$Global_reactive_power)
+data$Voltage<-as.numeric(data$Voltage)   
+data$Global_intensity<-as.numeric(data$Global_intensity)
+data$Sub_metering_1<-as.numeric(data$Sub_metering_1)
+data$Sub_metering_2<-as.numeric(data$Sub_metering_2)
+
+#Create the subdata
+HPC<-subset(data,Date<"2007-02-03")
+HPC<-subset(HPC,Date>="2007-02-01")
+
+#Remove the original data
+rm(data)
+
+#Create the second Plot
+library(png)
+attach(HPC)
+png("Plot2.png",width=480,height=480)
+plot(Date,Global_active_power,type='l',xlab='',ylab="Global Active Power (kilowatts)")
+dev.off()
